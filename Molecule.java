aa6223b629b5b5df78807d81470502aaa3490e2d
dimerize <53397413+dimerize@users.noreply.github.com>
2019-07-28 16:35:22 -0400

    Update Molecule.java

--- Molecule.java
+++ Molecule.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author o518316k
- */
 public class Molecule {
     private int numberOfCarbons;
     private int numberOfHydrogens;
